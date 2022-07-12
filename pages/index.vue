babba7267ba7f0ac8e5365e784ab478b5d063167
竹岡浩之 <tapioca_hiroyuki@mac.com>
2022-07-12 10:30:29 +0900

    送信確認ボタンを押すとテストメールが送信されることを確認

--- pages/index.vue
+++ pages/index.vue
@@ -16,13 +16,32 @@
 </template>
 
 <script>
+import Mailgun from "mailgun.js";
+import formData from "form-data";
+const mailgun = new Mailgun(formData);
+
 export default {
   name: "IndexPage",
   methods: {
     onSubmit(e) {
       e.preventDefault();
-      alert("投稿されました");
+      var mg = mailgun.client({
+        username: "api",
+        key: process.env.MAILGUN_API_KEY,
+      });
+      const domain = process.env.MAILGUN_DOMAIN;
+
+      mg.messages
+        .create(domain, {
+          from: "Excited User <mailgun@sandbox-123.mailgun.org>",
+          to: [process.env.MAILGUN_FROM_ADDRESS],
+          subject: "Hello",
+          text: "Testing some Mailgun awesomness!",
+          html: "<h1>Testing some Mailgun awesomness!</h1>",
+        })
+        .then((msg) => console.log(msg)) // logs response data
+        .catch((err) => console.error(err)); // logs any error
     },
-  },
+  }
 };
 </script>
